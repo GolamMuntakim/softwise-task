fb4833b1f78a6f15dc541268cec280ca7e7d3150
Golam Muntakim <seams123456789@gmail.com>
2024-09-01 22:52:59 -0700

    sale alert part done

---
+++ script.js
@@ -0,0 +1,12 @@
+// sale alert part
+const body = document.querySelector('body')
+const foot = document.createElement('div')
+const conatiner = body.appendChild(foot)
+conatiner.innerText = 'SALE ALERT: 30% off sitewide | Code : MEMORIAL - 20% Off Fine | COde: FINE20'
+conatiner.style.textAlign = 'center'
+conatiner.style.color = 'white'
+conatiner.style.backgroundColor = 'black'
+conatiner.style.padding = '10px'
+body.style.padding ='0px'
+body.style.margin ='0px'
+
